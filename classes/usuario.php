b05bf23700c8267cf7b9cf95ef47aea34b0bc343
Gerson Bernardo <gbds25@gmail.com>
2020-12-21 09:50:15 -0300

    Classe criada com Sucesso

---
+++ classes/usuario.php
@@ -0,0 +1,71 @@
+<?php
+
+class Usuario
+{
+    private $pdo;
+    public $msgErro = "";
+
+    public function conectar($nome, $host, $usuario, $senha)
+    {
+        global $pdo;
+        global $msgErro;
+        
+        try
+        {
+            $pdo = new PDO("mysql:dbname=".$nome.";host".$host, $usuario, $senha );
+        } catch (PDOException $e)
+        {
+            $msgErro = $e -> getMessage();
+        }
+
+    }
+
+    public function cadastrar($cpf, $nome, $email, $senha)
+    {
+        global $pdo;
+        global $msgErro;
+        //verificar se está cadastrado
+        $sql = $pdo->prepare("select sol_cpf from tab_solicitante where email= :e");
+        $sql->bindValue(":e", $email);
+        $sql->execute();
+        if($sql->rowCount() > 0)
+        {
+            return false; //Cadastro Existente
+        }
+        else()
+        {
+            //Caso não cadastrar
+            $sql = $pdo->prepare("insert into tab_solicitante (sol_cpf, sol_nome, sol_email, sol_senha) values (:c, :n, :e, :s)");
+            $sql->bindValue(":c", $cpf);
+            $sql->bindValue(":e", $email);
+            $sql->bindValue(":n", $nome);
+            $sql->bindValue(":s", $senha);
+            $sql->execute();
+            return true; // Cadastro Realizado
+        }
+    }
+
+    public function logar($email, $senha)
+    {
+        global $pdo;
+        global $msgErro;
+
+        // Verificar se email e senha estão cadastrados
+        $sql = $pdo->prepare("select sol_cpf from tab_solicitante where sol_emai = :e and senha = :s");
+        $sql->bindValue(":e", $email);
+        $sql->bindValue(":s", $senha);
+        $sql->execute();
+        if ($sql->rowcount() > 0) 
+        {
+            $dado = $sql->fetch();
+            session_start(); 
+            $_SESSION['sol_cpf'] = $dado['sol_cpf'];
+            return true; // logado com sucesso    
+        } 
+        else
+        {
+            return false; // Impossivel Logar
+        }
+    }
+}
+?>
